b5552eb7ce3adfab9e4115efc29e44720cb6f253
Vijaykumar Singh <singhvjd@gmail.com>
2026-05-08 20:46:54 -0500

    Move glob helpers into storage common

--- crates/storage/proximadb-storage-common/src/lib.rs
+++ crates/storage/proximadb-storage-common/src/lib.rs
@@ -4,7 +4,9 @@
 //! by multiple storage engines or modality storage implementations.
 
 pub mod bitmap;
+pub mod glob;
 pub mod storage_path;
 
 pub use bitmap::{BitmapError, BitmapIteratorAll, RoaringBitmap};
+pub use glob::{GlobError, GlobMatcher, GlobPattern, glob_match};
 pub use storage_path::StoragePath;
